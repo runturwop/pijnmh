d8e8836be06df5ab10a1194eb877321343f6e464
runturwop <96803635+runturwop@users.noreply.github.com>
2023-03-15 13:05:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=edc2f07e-266d-4672-be38-39b426bb2fcc
-CADDYIndexPage=https://github.com/wulabing/3DCEList/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/kk3948/arsegugd/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/kk3948/arsegugd/master/etc/xray.json
+AUUID=ff8ae8b7-2885-4e2f-a972-de9f519027d2
+CADDYIndexPage=https://github.com/runturwop/3dwokoer/raw/main/3DCEList.zip
+CONFIGCADDY=https://raw.githubusercontent.com/runturwop/pijnmh/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/runturwop/pijnmh/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/kk3948/arsegugd/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/runturwop/pijnmh/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
